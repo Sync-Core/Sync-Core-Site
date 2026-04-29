0c610985db943764eb92d6dbab70f27cf0bd39de
Sivamani-18 <siva@mindwaveventures.com>
2026-04-29 06:05:42 +0000

    Synced Build: Wed Apr 29 06:05:42 UTC 2026

--- site/_next/static/53LrSNjmdnvgA0uosTS8I/_buildManifest.js
+++ site/_next/static/53LrSNjmdnvgA0uosTS8I/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Sync-Core-Site//_next/:path+",
+        "destination": "/Sync-Core-Site/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
